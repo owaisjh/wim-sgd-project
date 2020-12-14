73b7bd11e7738d279519266854fb58a6a9ed6b81
Owais Hetavkar <owaisjh12@gmail.com>
2020-12-14 10:57:57 +0530

    links changed

--- src/components/Landmarks.tsx
+++ src/components/Landmarks.tsx
@@ -45,7 +45,7 @@ function Roads (props: { Back: ((event: React.MouseEvent<SVGSVGElement, MouseEve
       }
 
       function sendPostgresql(data: any){
-        const response = fetch('https://39b62732f5fe.ngrok.io/storeLandmark', {  //Hosted Apis on localhost:5000
+        const response = fetch('https://3d45b39cec12.ngrok.io/storeLandmark', {  //Hosted Apis on localhost:5000
           method: 'POST',
           headers:{
             'Content-Type' : 'application/json',
@@ -57,7 +57,7 @@ function Roads (props: { Back: ((event: React.MouseEvent<SVGSVGElement, MouseEve
 
       function sendNeo4j(data: any){
     
-        const response = fetch( 'https://39b62732f5fe.ngrok.io/add_landmark' ,{  //Hosted Apis on localhost:5000
+        const response = fetch( 'https://3d45b39cec12.ngrok.io/add_landmark' ,{  //Hosted Apis on localhost:5000
           method: 'POST',
           headers:{
             'Content-Type' : 'application/json',
